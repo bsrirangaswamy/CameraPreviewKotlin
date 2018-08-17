bc97f507bf1aae0166a2eb8edba64f7f15778dcc
bsrirangaswamy <30527688+bsrirangaswamy@users.noreply.github.com>
2018-08-17 16:20:30 -0500

    UI Updates
    => Added "Buffering..." text to indicate that a file is being prepared before the video starts playing.
    => Play video in both portrait and landscape.

--- app/src/main/java/com/priyabala/bala/androidmanchild/VideoImagePreviewActivity.kt
+++ app/src/main/java/com/priyabala/bala/androidmanchild/VideoImagePreviewActivity.kt
@@ -6,19 +6,36 @@ import android.os.Build
 import android.os.Bundle
 import android.widget.MediaController
 import kotlinx.android.synthetic.main.activity_video_image_preview.*
+import android.webkit.URLUtil
+import android.widget.Toast
+import android.widget.VideoView
 
 class VideoImagePreviewActivity : Activity() {
     private var videoStringPath: String? = null
     private var imageStringPath: String? = null
+    private var mCurrentVideoPosition: Int = 0
+    private val PLAYBACK_TIME = "play_time"
+    private val VIDEO_SAMPLE = "https://developers.google.com/training/images/tacoma_narrows.mp4"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_image_preview)
         videoStringPath = intent.getStringExtra(EXTRA_VIDEO_PATH)
         imageStringPath = intent.getStringExtra(EXTRA_IMAGE_PATH)
+        if (savedInstanceState != null) {
+            mCurrentVideoPosition = savedInstanceState.getInt(PLAYBACK_TIME)
+            println("Bala log: current video position 1 = $mCurrentVideoPosition")
+        }
+        println("Bala log: current video position 2 = $mCurrentVideoPosition")
         if (videoStringPath != null) {
             setupVideo()
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putInt(PLAYBACK_TIME, video_view.currentPosition)
+        println("Bala log: video position saved = " + video_view.currentPosition)
     }
 
     override fun onStart() {
@@ -49,9 +66,31 @@ class VideoImagePreviewActivity : Activity() {
     }
 
     private fun initializer() {
+        videoStringPath = VIDEO_SAMPLE //Test
         val videoPath = videoStringPath ?: return
+//        if (URLUtil.isValidUrl(videoPath)) {
+        text_View.visibility = VideoView.VISIBLE
         val videoUri = getMedia(videoPath)
-        video_view.setVideoURI(videoUri)
+            video_view.setVideoURI(videoUri)
+
+            video_view.setOnCompletionListener {
+                Toast.makeText(this, "Playback completed", Toast.LENGTH_SHORT).show();
+                video_view.seekTo(1);
+            }
+
+            video_view.setOnPreparedListener {
+                text_View.visibility = VideoView.INVISIBLE
+                startVideo()
+            }
+//        }
+    }
+
+    private fun startVideo() {
+        if (mCurrentVideoPosition > 0) {
+            video_view.seekTo(mCurrentVideoPosition)
+        } else {
+            video_view.seekTo(1)
+        }
         video_view.start()
     }
 
